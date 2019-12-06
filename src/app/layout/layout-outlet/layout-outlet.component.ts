aad63f5944372b6e8d09659eaa2bd1026c6bf813
Mohanraj Velusamy <mvh5cob@rbeigcn.com>
2019-12-06 14:44:44 +0530

    Home component

--- src/app/layout/layout-outlet/layout-outlet.component.ts
+++ src/app/layout/layout-outlet/layout-outlet.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -6,16 +6,21 @@ declare var $: any;
   templateUrl: './layout-outlet.component.html',
   styleUrls: ['./layout-outlet.component.css']
 })
-export class LayoutOutletComponent implements OnInit {
+export class LayoutOutletComponent implements OnInit, OnDestroy {
 
   constructor() { }
 
   ngOnInit() {
-    debugger;
     $('body').layout('fix');
     const trees: any = $('[data-widget="tree"]');
     trees.tree();
     $('body').removeClass('login-page');
+    $('body').addClass('hold-transition skin-midnight');
+  }
+
+  ngOnDestroy() {
+    $('body').addClass('login-page');
+    $('body').removeClass('hold-transition skin-midnight');
   }
 
   LogoutFunction() {
