1a8698f8cd82e0ea05253d904fefb007137bc39c
Mohanraj <mvh5cob@rbeigcn.com>
2019-12-07 12:16:18 +0530

    design change

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from 'src/app/shared/services';
 
 @Component({
   selector: 'app-header',
@@ -7,7 +9,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService) { }
 
   ngOnInit() {
   }
@@ -16,6 +20,8 @@ export class HeaderComponent implements OnInit {
   }
 
   Logout() {
+    this.authenticationService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
